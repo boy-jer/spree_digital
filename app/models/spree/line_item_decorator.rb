5f1a0872bfceea229c2d583523fa8b241b2285b3
drefined <d.refined@gmail.com>
2012-04-06 12:16:55 -0700

    remove shipment method check on save and destroy

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -3,7 +3,6 @@ Spree::LineItem.class_eval do
   has_many :digital_links, :dependent => :destroy
 
   after_save :create_digital_links, :if => :digital?
-  after_destroy :check_shipping_method
 
   # Is this item digital?
   def digital?
@@ -18,14 +17,6 @@ Spree::LineItem.class_eval do
 
     self.quantity.times do
       digital_links.create!(:digital => variant.digital)
-    end
-  end
-
-  def check_shipping_method
-    if order.digital?
-      if order.shipment.present?
-        order.shipment.update_attributes(:shipping_method_id => order.digital_shipping_method[:id])
-      end
     end
   end
 
